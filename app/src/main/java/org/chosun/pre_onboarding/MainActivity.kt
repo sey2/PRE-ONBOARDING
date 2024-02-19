264457169b74dc07c76b68c648876c4530635a20
casio2978 <casio2978@naver.com>
2024-02-19 13:29:05 +0900

    design: 4 - UI 조정하기

--- app/src/main/java/org/chosun/pre_onboarding/MainActivity.kt
+++ app/src/main/java/org/chosun/pre_onboarding/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.chosun.pre_onboarding.ui.theme.Pre_onboardingTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(24.dp)
     )
 }
 
