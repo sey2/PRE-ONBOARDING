03b5eae4bf5c5cd6bcef864020322fa310996c7c
casio2978 <casio2978@naver.com>
2024-02-20 17:34:59 +0900

    design: 7 - Compose State

--- app/src/main/java/org/chosun/pre_onboarding/MainActivity.kt
+++ app/src/main/java/org/chosun/pre_onboarding/MainActivity.kt
@@ -13,10 +13,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.chosun.pre_onboarding.ui.theme.Pre_onboardingTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,23 +50,30 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded = remember { mutableStateOf(false) }
+    var extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
