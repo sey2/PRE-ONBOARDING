fddf38d81a1bb17de2974094b9791789e4663688
casio2978 <casio2978@naver.com>
2024-02-23 16:07:48 +0900

    feat: 2 - async

--- coroutine/sync.kt
+++ coroutine/sync.kt
@@ -1,22 +1,25 @@
-import kotlin.system.*
 import kotlinx.coroutines.*
 
 fun main() {
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            printForecast()
-            printTemperature()
-        }
+    runBlocking {
+        println("Weather forecast")
+        println(getWeatherReport())
+        println("Have a good day!")
     }
-    println("Execution time: ${time / 1000.0} seconds")
-}
-suspend fun printForecast() {
-    delay(1000)
-    println("Sunny")
 }
 
-suspend fun printTemperature() {
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+    "${forecast.await()} ${temperature.await()}"
+}
+
+suspend fun getForecast(): String {
     delay(1000)
-    println("30\u00b0C")
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    return "30\u00b0C"
 }
